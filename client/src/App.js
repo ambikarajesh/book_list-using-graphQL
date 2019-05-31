e7d671f3dc76e7891bc707724c210d234b739ac0
Ambika <ambikula@gmail.com>
2019-05-30 17:31:53 -0700

    set apollo cllient in front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import BookList from './components/bookList/bookList';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+const client = new ApolloClient({
+  uri:'http://localhost:8080/graphql'
+})
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>Book List</h1>
+          <BookList/>
+        </div>
+    </ApolloProvider>
+    );
+  }
 }
 
 export default App;
+
---
+++ client/src/components/bookList/bookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class bookList extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Book name</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default bookList;
